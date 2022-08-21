0172d04b3c806bcf8bd368ab4e7cfc750e20bfac
Yevhenii Kyrykoi <kirikoy1@gmail.com>
2022-08-21 08:31:19 +0300

    add some cheking in LinkedList

--- src/demo2/ListTest.java
+++ src/demo2/ListTest.java
@@ -4,8 +4,25 @@ public class ListTest {
 
 	public static void main(String[] args) {
 		
-		Integer result = 22;
-		System.out.println(result);
+		LinkedList nodeLinkedList = new LinkedList();
+		nodeLinkedList.add(100);
+		nodeLinkedList.add(200);
+		nodeLinkedList.add(300);
+
+		System.out.println(nodeLinkedList.get(1));
+		System.out.println(nodeLinkedList.get(2));
+		System.out.println(nodeLinkedList.get(3)+"\n");
+		
+		System.out.println(nodeLinkedList.size()+"\n");
+		
+		System.out.println(nodeLinkedList.delete(2)+"\n");
+		
+		System.out.println(nodeLinkedList.get(1));
+		System.out.println(nodeLinkedList.get(2));
+		System.out.println(nodeLinkedList.get(3)+"\n");
+		
+		System.out.println(nodeLinkedList.size());
+		
 	}
 
 }
